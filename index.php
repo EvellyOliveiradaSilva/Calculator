94a7807b0ddc2f58143ca7766f7f36cf2d323329
Evelly Silva <evelly.oliveiradasilva@hotmail.com]>
2021-04-07 14:40:21 -0300

    codigo calculator aprimorado html e php

--- index.php
+++ index.php
@@ -12,26 +12,35 @@ $resultado = 0;
 if($operacao == 'soma'){
     $resultado = $numberOne+$numberTwo;
 }
-else if($operacao=='subtracao'){
+else if($operacao=='subtração'){
     $resultado = $numberOne-$numberTwo;
 }
-else if($operacao=='divisao'){
+else if($operacao=='divisão'){
     $resultado = $numberOne/$numberTwo;
 }
-else if($operacao=='multiplicacao'){
+else if($operacao=='multiplicação'){
     $resultado = $numberOne*$numberTwo;
 }
 
 
 ?>
 
-<b>Número1</b>: <?php echo $numberOne; ?><br/>
-<b>Número2</b>: $numberTwo <br/>
-<b>Operação</b>: $operacao <br/>
-<b>Resultado</b>: $resultado
+<b>Número1</b>: <?php echo $numberOne; ?> <br/>
+<b>Número2</b>: <?php echo $numberTwo; ?> <br/>
+<b>Operação</b>: <?php echo $operacao; ?> <br/>
+<b>Resultado</b>: <?php echo $resultado; ?>  <br/>
 
+<form action='http://localhost/Calculator/'>
 
-<br/>
-<br/>
-<br/>
+    <label for="numberOne">1° Número:</label>
+    <input type="number" id="numberOne" name="number1" value="<?php echo $numberOne; ?>" ><br/>
+    <label for="numberTwo">2° Número:</label>
+    <input type="number" id="numberTwo" name="number2" value="<?php echo $numberTwo; ?>" ><br/>
+    <br/>
 
+    <input type="submit" value="soma" name="operacao"/><br/>
+    <input type="submit" value="subtração" name="operacao" /><br/>
+    <input type="submit" value="divisão" name="operacao" /><br/>
+    <input type="submit" value="multiplicação" name="operacao" /><br/>
+
+</form>
